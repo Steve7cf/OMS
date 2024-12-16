07d3c7a6eb53c29954eb3f01bf1f76bf5f83e223
stephano mwita <stevebazaar99@gmail.com>
2024-12-16 16:59:16 +0300

    main

--- routes/routes.js
+++ routes/routes.js
@@ -8,7 +8,7 @@ const signupAuthentication = require('../middlewares/signupAuthentication')
 // get routes
 route.get('/', rest.index)
  
-route.get('/order',userAuthentication("user"), rest.order)
+route.get('/order',userAuthentication("user"), rest.order) //user
 route.get('/createAdmin', rest.createAdmin)
 
 
@@ -17,28 +17,35 @@ route.post("/auth", rest.auth)
 route.post("/create", rest.create)
 route.patch("/updateUser", rest.updateUser)
 route.delete("/deleteUser", rest.deleteUser)
-route.get("/signup",signupAuthentication("user"), rest.signup)
 route.get("/login", rest.login)
 route.get("/admin", rest.adminPanel)
-
-// admin routes
 route.post("/authadmin", rest.authAdmin)
-route.get('/dashboard',adminAuthentication("admin"), rest.dashboard)
+route.get('/orderPage',userAuthentication("user"), rest.orderPage)
+
+// user authentication
+route.get("/logoutUser",userAuthentication("user"), rest.logoutUser) //user
+route.get("/signup",signupAuthentication("user"), rest.signup) //user
+route.post("/createOrder",userAuthentication("user"), rest.createOrder) //user
+route.patch("/updateOrder",userAuthentication("user"), rest.updateOrder) //user
 
 
-// order routes
-route.post("/createOrder",userAuthentication("user"), rest.createOrder)
-route.patch("/updateOrder",adminAuthentication("user"), rest.updateOrder)
-route.delete("/deleteOrder",adminAuthentication("admin"), rest.deleteOrder)
-route.get('/orderPage', rest.orderPage)
+// admin authentication
+route.get('/dashboard',adminAuthentication("admin"), rest.dashboard) //admin
+route.delete("/deleteOrder",adminAuthentication("admin"), rest.deleteOrder) //admin
+route.get("/logoutAdmin",adminAuthentication("admin"), rest.logoutAdmin) //admin 
+route.post('/allToken',adminAuthentication("admin"), rest.allToken) //admin
+route.post('/genToken',adminAuthentication("admin"), rest.genToken) //admin
+
+
+
+
 
 // other routes
 route.get('/token', rest.token) 
 route.post('/verifyToken', rest.verifyToken)
-route.post('/allToken', rest.allToken) //admin
-route.post('/genToken', rest.genToken) //admin
-route.get("/logoutUser",userAuthentication("user"), rest.logoutUser)
-route.get("/logoutAdmin",adminAuthentication("admin"), rest.logoutAdmin)
+
+
+
 
 
 module.exports = route
